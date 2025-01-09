e1710bdfa181d43d872d79600ad5782440bd85c6
Oyelola Victoria <oyelolaabimbola23@gmail.com>
2025-01-09 13:23:19 +0100

    redirect to "/new" after signUp or signIn

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -6,7 +6,7 @@ export default function SignInPage() {
             <div className="z-0 absolute top-[-50%] left-[-50%] right-[-50%] bottom-[-50%] h-[200vh] w-[200svw] animate-noise"></div>
             <div className="z-[-1] absolute -top-[20svh] -left-[10svw] w-[200px] h-[200px] lg:w-[500px] lg:h-[500px] bg-primary blur-[50px] lg:blur-[100px] rounded-full"></div>
             <div className="z-[-1] absolute -bottom-[20svh] -right-[10svw] w-[200px] h-[200px] lg:w-[500px] lg:h-[500px] bg-primary blur-[50px] lg:blur-[100px] rounded-full"></div>
-            <SignIn />
+            <SignIn forceRedirectUrl="/new" />
         </main>
     );
 }
