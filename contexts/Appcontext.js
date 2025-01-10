4dd8f3c25b468dd1602ed9cd9d59e3bfd85f2595
Oyelola Victoria <oyelolaabimbola23@gmail.com>
2025-01-10 08:42:08 +0100

    sync signed in users with firebase

--- contexts/Appcontext.js
+++ contexts/Appcontext.js
@@ -1,11 +1,29 @@
 "use client";
 import useViewport from "@/hooks/useViewport";
-import { createContext } from "react";
-
+import { createContext, useEffect } from "react";
+import { useAuth } from "@clerk/nextjs";
+import { signInWithCustomToken } from "firebase/auth";
+import { auth } from "@/config/firebase";
 export const AppContext = createContext(null);
 
 export default function AppContextProvider({ children }) {
   const [viewport] = useViewport();
+  const { getToken, isSignedIn } = useAuth();
+
+  useEffect(() => {
+    async function signInToFirebase() {
+      try {
+        const token = await getToken({ template: "integration_firebase" });
+        await signInWithCustomToken(auth, token);
+      } catch (error) {
+        alert(error.message);
+      }
+    }
+
+    if (isSignedIn) {
+      signInToFirebase();
+    }
+  }, [isSignedIn]);
 
   return (
     <AppContext.Provider value={{ viewport: viewport }}>
